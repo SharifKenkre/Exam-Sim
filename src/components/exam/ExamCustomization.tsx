fe0c1766d94974839d4ed83ac5dc3fe637629dd3
Sharif Kenkre <sharifkenkre22@gmail.com>
2025-09-15 14:30:12 +0000

    why i cant select the chapter

--- src/components/exam/ExamCustomization.tsx
+++ src/components/exam/ExamCustomization.tsx
@@ -56,6 +56,7 @@ export function ExamCustomization({ subject, chapters, maxQuestions, onStartExam
         ? prev.filter(c => c !== chapter) 
         : [...prev, chapter]
     );
+    setOpenChapterSelector(false);
   };
 
   const handleStartClick = () => {
